b4f70c94ffc151e033952bba6cc5644ab25551c5
h-sugawara <nnfxt540@gmail.com>
2014-03-06 09:40:25 +0900

    changed di wrapper unittest class

--- tests/DIPresetServicesTest.php
+++ tests/DIPresetServicesTest.php
@@ -2,6 +2,7 @@
 namespace Phpingguo\CitronDI\Tests;
 
 use Phpingguo\CitronDI\DIPresetServices;
+use Symfony\Component\Yaml\Parser;
 
 class DIPresetServicesTest extends \PHPUnit_Framework_TestCase
 {
@@ -17,10 +18,16 @@ class DIPresetServicesTest extends \PHPUnit_Framework_TestCase
      */
     public function testGetClass($group_name, $count, $expected)
     {
-        $services = DIPresetServices::get($group_name);
+        $group_path = realpath(__DIR__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config';
+        $services   = DIPresetServices::get($group_name, $group_path);
         
         $this->assertNotEmpty($services);
         $this->assertCount($count, $services);
         $this->assertSame($expected, $services);
+        
+        $path  = $group_path . DIRECTORY_SEPARATOR . "{$group_name}_preset_services.yml";
+        $value = is_file($path) ? (new Parser())->parse(file_get_contents($path)) : null;
+        
+        $this->assertSame($value, $services);
     }
 }
